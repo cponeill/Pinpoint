4e817deb8b4bb39e41bce6617b6201ca4ac427a1
cponeill <caseyoneill78@hotmail.com>
2016-12-30 03:21:44 -0500

    adding comment code in tests.py file.

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# Adding in unittests for Pinpoint microservice
+
 import os
 import unittest
 import incomeServer
@@ -8,14 +10,17 @@ from unittest import mock
 
 class PinpointTestCase(unittest.TestCase):
 
+    # getting app to test url
     def get(self, url):
         self.app = incomeServer.app.test_client()
         return self.app.get(url)
 
+    # testing whether 402 endpoint is called by the client
     def test_success(self):
         response = self.get('/get?ip=' + 'localhost')
         return self.assertEqual(response.status_code, 402)
 
+    # testing whether 200 endpoint is returned after payment received by 402
     @mock.patch('two1.bitserv.flask.decorator.Payment.contains_payment',
                 return_value=True)
     def test_buy_success(self, *args):
