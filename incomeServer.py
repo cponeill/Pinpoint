c582491a9d0bf8bb45919aa44acd30a4a762dacf
cponeill <caseyoneill78@hotmail.com>
2017-01-19 11:32:56 -0500

    Adding comment code to main function in server code.

--- incomeServer.py
+++ incomeServer.py
@@ -35,7 +35,10 @@ key = os.environ.get('KEY')
 @app.route('/get')
 @payment.required(1000)
 def get_endpoint():
-
+    """
+    Input: IP address
+    Output: JSON with geo-location and average income details of the IP address. 
+    """
     ip = request.args.get('ip')
     response = get_object.FetchData().income_by_ip(key)
     return response
