dce10b1b01c1eee5b2244c7291f7892feff820f3
cponeill <caseyoneill78@hotmail.com>
2017-01-25 10:58:25 -0500

    Keeping the comment code up to date.

--- tests.py
+++ tests.py
@@ -13,8 +13,7 @@ __maintainer__ = "cponeill"
 __email__ = "cponeill@blockshare.io"
 
 #!/usr/bin/env python3
-# Adding in unittests for Pinpoint microservice
-
+# Adding in unittests to test Pinpoint microservice.
 import os
 import unittest
 import incomeServer
@@ -24,17 +23,17 @@ from unittest import mock
 
 class PinpointTestCase(unittest.TestCase):
 
-    # getting app to test url
+    # Getting app to test url
     def get(self, url):
         self.app = incomeServer.app.test_client()
         return self.app.get(url)
 
-    # testing whether 402 endpoint is called by the client
+    # Testing whether 402 endpoint is called by the client
     def test_success(self):
         response = self.get('/get?ip=' + 'localhost')
         return self.assertEqual(response.status_code, 402)
 
-    # testing whether 200 endpoint is returned after payment received by 402
+    # Testing whether 200 endpoint is returned after payment received by 402
     @mock.patch('two1.bitserv.flask.decorator.Payment.contains_payment',
                 return_value=True)
     def test_buy_success(self, *args):
