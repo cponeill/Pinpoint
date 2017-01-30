6eb42204520278c855764b6e8bd0c67464bda995
cponeill <caseyoneill78@hotmail.com>
2017-01-30 12:47:25 -0500

    Cleaning client code to pep8 standards.

--- client.py
+++ client.py
@@ -1,27 +1,12 @@
-"""
-Copyright (c) 2016-2017 Blockshare Technologies, LLC.
-  ____  _            _     ____  _                      ___ ___  
- | __ )| | ___   ___| | __/ ___|| |__   __ _ _ __ ___  |_ _/ _ \ 
- |  _ \| |/ _ \ / __| |/ /\___ \| '_ \ / _` | '__/ _ \  | | | | |
- | |_) | | (_) | (__|   <  ___) | | | | (_| | | |  __/_ | | |_| |
- |____/|_|\___/ \___|_|\_\|____/|_| |_|\__,_|_|  \___(_)___\___/ 
-"""
-
-__author__ = "cponeill"
-__version__ = "0.01"
-__maintainer__ = "cponeill"
-__email__ = "cponeill@blockshare.io"
-
-
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 from two1.wallet import Wallet
 from two1.bitrequests import BitTransferRequests
 
-# Configure wallet and requests library
+# Configure wallet and requests library and assigning IPV6/port to variable
 wallet = Wallet()
 requests = BitTransferRequests(wallet)
-
 server_url = 'http://[::]:9876/'
+
 
 def get_data():
     """
@@ -32,6 +17,7 @@ def get_data():
     sel_url = server_url+'get?ip={0}'
     response = requests.get(url=sel_url.format(ip_addr))
     print(response)
+
 
 if __name__ == '__main__':
     get_data()
