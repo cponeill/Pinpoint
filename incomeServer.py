20c127fe86c090c6b969d8a1fba8aa6fb329375e
cponeill <caseyoneill78@hotmail.com>
2017-01-31 03:35:16 -0500

    server up to date to pep8 standards.

--- incomeServer.py
+++ incomeServer.py
@@ -1,19 +1,4 @@
-"""
-Copyright (c) 2016-2017 Blockshare Technologies, LLC.
-  ____  _            _     ____  _                      ___ ___  
- | __ )| | ___   ___| | __/ ___|| |__   __ _ _ __ ___  |_ _/ _ \ 
- |  _ \| |/ _ \ / __| |/ /\___ \| '_ \ / _` | '__/ _ \  | | | | |
- | |_) | | (_) | (__|   <  ___) | | | | (_| | | |  __/_ | | |_| |
- |____/|_|\___/ \___|_|\_\|____/|_| |_|\__,_|_|  \___(_)___\___/ 
-"""
-
-__author__ = "cponeill"
-__version__ = "0.01"
-__maintainer__ = "cponeill"
-__email__ = "cponeill@blockshare.io"
-
-#!/usr/bin/env python3
-
+#! /usr/bin/env python3
 import json
 import os
 
@@ -23,6 +8,7 @@ from two1.bitserv.flask import Payment
 
 import fetch_data as get_object
 
+
 # Configure the app and wallet
 app = Flask(__name__)
 wallet = Wallet()
@@ -31,17 +17,20 @@ payment = Payment(app, wallet)
 # Load the key
 key = os.environ.get('KEY')
 
+
 # Call the "FetchData" object and charge 1000 satoshi per request
 @app.route('/get')
 @payment.required(1000)
 def get_endpoint():
     """
     Input: IP address
-    Output: JSON with geo-location and average income details of the IP address. 
+    Output: JSON with geo-location and average income
+            details of the IP address.
     """
     ip = request.args.get('ip')
     response = get_object.FetchData().income_by_ip(key)
     return response
+
 
 if __name__ == '__main__':
     app.run('::', port=9876)
